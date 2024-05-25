394ec1e0396528d0f9d544da1cc33ff2dbf4d37e
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-05-25 02:31:04 -0700

    Add comments

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function getComputerChoice() {
+  // Get a random number from 1 to 3 for computerChoice
   let computerChoice = Math.floor(Math.random() * 3) + 1;
+
   if (computerChoice == 1) {
     return 'rock';
   } else if (computerChoice == 2) {
@@ -10,16 +12,20 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
+  // Get a humanChoice by prompt()
   let humanChoice = prompt('What is your move? (rock, paper, scissors)');
   return humanChoice;
 }
 
+// Initialize humanScore and computerScore to 0
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
+  // Make humanChoice parameter case-insensitive
   humanChoice = humanChoice.toLowerCase();
 
+  // Outputs the round winner and increments score
   if (humanChoice == computerChoice) {
     console.log('This round is a draw!');
   } else if (humanChoice == 'rock' && computerChoice == 'paper') {
@@ -43,10 +49,13 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
+// Initialize humanSelection and computerSelection to empty string
 let humanSelection = '';
 let computerSelection = '';
 
 function playGame() {
+
+  // Loop 5 rounds by calling playRound 5 times
   for (let round = 0; round < 5; round++ ) {
     humanSelection = getHumanChoice();
     computerSelection = getComputerChoice();
