120b3d6dd4b7b63a852d6b7584eeffe015dc9230
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-05-25 02:09:04 -0700

    Add playRound()

--- script.js
+++ script.js
@@ -16,3 +16,35 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  console.log(computerChoice);
+
+  if (humanChoice == computerChoice) {
+    console.log('This round is a draw!');
+  } else if (humanChoice == 'rock' && computerChoice == 'paper') {
+    console.log('You lose! Paper beats Rock');
+    computerScore++
+  } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+    console.log('You win! Rock beats Scissors');
+    humanScore++;
+  } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+    console.log('You lose! Scissors beats Paper');
+    computerScore++;
+  } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+    console.log('You win! Paper beats Rock');
+    humanScore++;
+  } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+    console.log('You lose! Rock beats Scissors');
+    computerScore++;
+  } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+    console.log('You win! Scissors beats Paper');
+    humanScore++;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
